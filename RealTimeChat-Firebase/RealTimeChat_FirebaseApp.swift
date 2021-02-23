7185650ce94e3bd8a74265f076234add5d370df4
Michael Svendsen <michael@mikesvendsen.net>
2021-02-23 15:55:51 -0500

    Initial Commit

---
+++ RealTimeChat-Firebase/RealTimeChat_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeChat_FirebaseApp.swift
+//  RealTimeChat-Firebase
+//
+//  Created by Michael Svendsen on 2021-02-23.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeChat_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
