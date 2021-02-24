d7eba25d91eeb543480425eb9704917331cab3d6
Michael Svendsen <michael@mikesvendsen.net>
2021-02-24 15:00:26 -0500

    Removed Pods

--- Pods/Target Support Files/Pods-RealTimeChat-Firebase-RealTimeChat-FirebaseUITests/Pods-RealTimeChat-Firebase-RealTimeChat-FirebaseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RealTimeChat_Firebase_RealTimeChat_FirebaseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RealTimeChat_Firebase_RealTimeChat_FirebaseUITests
-@end
--- Pods/Target Support Files/Pods-RealTimeChat-FirebaseTests/Pods-RealTimeChat-FirebaseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RealTimeChat_FirebaseTests : NSObject
-@end
-@implementation PodsDummy_Pods_RealTimeChat_FirebaseTests
-@end
